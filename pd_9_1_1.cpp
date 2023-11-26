a4cc5781bf9b18fcad02fafe53ff1b94950defce
Shawn Halayka <sjhalayka@gmail.com>
2023-11-25 18:21:35 -0600

    Add files via upload

---
+++ pd_9_1_1.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+string ascii_caesar_encrypt(const string& input)
+{
+	string ret = input;
+
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		char c = input[i] + 7;
+
+		if (c != ' ' && c > 'Z')
+			c -= 26;
+
+		ret[i] = c;
+	}
+
+	return ret;
+}
+
+string ascii_caesar_decrypt(const string& input)
+{
+	string ret = input;
+
+	for (size_t i = 0; i < input.size(); i++)
+	{
+		char c = input[i] - 7;
+
+		if (c != ' ' && c < 'A')
+			c += 26;
+
+		ret[i] = c;
+	}
+
+	return ret;
+}
+
+
+int main(void)
+{
+	// One could use RSA without padding, which is technically a glorified Caesar cipher
+	// Instead, we will implement the encryption/decryption wheel in Figure 9.2
+
+	string plaintext = "HELLO WORLD";
+	string encrypted_text = ascii_caesar_encrypt(plaintext);
+	string decrypted_text = ascii_caesar_decrypt(encrypted_text);
+
+	cout << plaintext << endl << encrypted_text << endl << decrypted_text << endl;
+
+	return 0;
+}
