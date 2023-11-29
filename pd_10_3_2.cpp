649d51958e3274fd452a5634dbc756c013631267
Shawn Halayka <sjhalayka@gmail.com>
2023-11-29 16:56:59 -0600

    Add files via upload

--- pd_10_3_2.cpp
+++ pd_10_3_2.cpp
@@ -1,42 +1,38 @@
-// https://web.stanford.edu/class/archive/cs/cs106b/cs106b.1186/assn/huffman.html
-
-
-
 #include <iostream>
 #include <queue>
 #include <map>
 #include <vector>
-
 using namespace std;
 
-// Define a structure for a Huffman tree node
+
+// Modified from ChatGPT code
 struct HuffmanNode
 {
-    char datum;
+    char data;
     int frequency;
+
     HuffmanNode* left;
     HuffmanNode* right;
 
-    HuffmanNode(char datum, int frequency) : datum(datum), frequency(frequency), left(nullptr), right(nullptr) {}
+    HuffmanNode(char data, int frequency) : data(data), frequency(frequency), left(nullptr), right(nullptr)
+    {
+    
+    }
 };
 
 vector<HuffmanNode*> pointers_to_clean_up;
 
-// Comparison function for priority queue
-struct CompareNodes
-{
+struct CompareNodes {
     bool operator()(HuffmanNode* lhs, HuffmanNode* rhs)
     {
         return lhs->frequency > rhs->frequency;
     }
 };
 
-// Function to build the Huffman tree
-HuffmanNode* buildHuffmanTree(map<char, int>& frequencies) 
+HuffmanNode* buildHuffmanTree(map<char, int>& frequencies)
 {
     priority_queue<HuffmanNode*, vector<HuffmanNode*>, CompareNodes> pq;
 
-    // Create a leaf node for each character and add it to the priority queue
     for (const auto& entry : frequencies)
     {
         HuffmanNode* t = new HuffmanNode(entry.first, entry.second);
@@ -44,8 +40,7 @@ HuffmanNode* buildHuffmanTree(map<char, int>& frequencies)
         pq.push(t);
     }
 
-    // Build the Huffman tree by repeatedly combining the least frequent nodes
-    while (pq.size() > 1) 
+    while (pq.size() > 1)
     {
         HuffmanNode* left = pq.top();
         pq.pop();
@@ -53,31 +48,28 @@ HuffmanNode* buildHuffmanTree(map<char, int>& frequencies)
         HuffmanNode* right = pq.top();
         pq.pop();
 
-        HuffmanNode* newNode = new HuffmanNode('$', left->frequency + right->frequency);
+        HuffmanNode* newNode = new HuffmanNode('\0', left->frequency + right->frequency);
         newNode->left = left;
         newNode->right = right;
         pointers_to_clean_up.push_back(newNode);
         pq.push(newNode);
     }
 
-    // The remaining node is the root of the Huffman tree
     return pq.top();
 }
 
-// Function to traverse the Huffman tree and generate Huffman codes
 void generateCodes(HuffmanNode* root, string code, map<char, string>& codes)
 {
-    if (root == nullptr)
+    if (root == nullptr) 
         return;
 
-    if (root->datum != '$')
-        codes[root->datum] = code;
+    if (root->data != '\0')
+        codes[root->data] = code;
 
     generateCodes(root->left, code + "0", codes);
     generateCodes(root->right, code + "1", codes);
 }
 
-// Function to encode a message using Huffman codes
 string encode(string message, map<char, string>& codes)
 {
     string encodedMessage = "";
@@ -88,7 +80,6 @@ string encode(string message, map<char, string>& codes)
     return encodedMessage;
 }
 
-// Function to decode a message using Huffman codes
 string decode(HuffmanNode* root, string encodedMessage)
 {
     string decodedMessage = "";
@@ -96,14 +87,14 @@ string decode(HuffmanNode* root, string encodedMessage)
 
     for (char bit : encodedMessage) 
     {
-        if (bit == '0') 
+        if (bit == '0')
             current = current->left;
-        else 
+        else
             current = current->right;
 
-        if (current->datum != '$') 
+        if (current->left == nullptr && current->right == nullptr)
         {
-            decodedMessage += current->datum;
+            decodedMessage += current->data;
             current = root;
         }
     }
@@ -117,32 +108,29 @@ void clean_up(void)
         delete pointers_to_clean_up[i];
 }
 
-int main()
-{
-    string message = "This is an example for huffman encoding!";
 
-    // Count the frequency of each character in the message
+
+int main(void) 
+{
+    string message = "this is an example for huffman encoding";
+
     map<char, int> frequencies;
 
     for (char c : message)
         frequencies[c]++;
 
-    // Build the Huffman tree
     HuffmanNode* root = buildHuffmanTree(frequencies);
 
-    // Generate Huffman codes for each character
     map<char, string> codes;
     generateCodes(root, "", codes);
 
-    // Encode the message
     string encodedMessage = encode(message, codes);
     cout << "Encoded Message: " << encodedMessage << endl;
 
-    // Decode the message
     string decodedMessage = decode(root, encodedMessage);
     cout << "Decoded Message: " << decodedMessage << endl;
 
     clean_up();
 
     return 0;
-}
+}   
