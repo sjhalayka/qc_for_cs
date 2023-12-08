3ebae3d63bba07d662e030cd8dfecbd9079a303e
Shawn Halayka <sjhalayka@gmail.com>
2023-12-08 14:11:49 -0600

    Add files via upload

--- pd_9_2_2.cpp
+++ pd_9_2_2.cpp
@@ -6,12 +6,12 @@ using namespace std;
 
 template<size_t n>
 void knuth2(
+	const vector<size_t> &test_agreed_indices,
 	const bitset<n>& bit_sent,
 	const bitset<n>& sending_basis,
 	const bitset<n>& receiving_basis,
 	bitset<n>& bit_received,
-	bitset<n>& agreed_bits,
-	size_t &agreed_bits_size)
+	bitset<n>& agreed_bits)
 {
 	for (size_t i = 0; i < n; i++)
 	{
@@ -22,8 +22,25 @@ void knuth2(
 	}
 
 	// Todo: get agreed_bits
-	agreed_bits_size = n;
+	size_t agreed_bits_size = test_agreed_indices.size();
 
+	size_t agreed_bits_index = 0;
+
+	for (size_t i = 0; i < test_agreed_indices.size(); i++)
+	{
+		const size_t index = test_agreed_indices[i];
+
+		if (bit_sent[index] == bit_received[index])
+		{
+			agreed_bits[agreed_bits_index] = 1;
+			agreed_bits_index++;
+		}
+		else
+		{
+			agreed_bits[agreed_bits_index] = 0;
+			agreed_bits_index++;
+		}
+	}
 }
 
 int main(void)
@@ -32,7 +49,7 @@ int main(void)
 	// looking for all 3 bit strings (e.g. alice_bit_sent, etc) or 
 	// all 3-bit strings (e.g. "000"), or both, so we do both!
 
-	const size_t n = 3; // number of bits per string
+	const size_t n = 8; //3; // number of bits per string
 
 	vector<bitset<n>> bit_sets;
 
@@ -58,17 +75,30 @@ int main(void)
 			bob_receiving_basis[j] = rand() % 2;
 
 		bitset<n> bob_bit_received;
+
+		vector<size_t> indices_for_agreement;
+
+		for (size_t j = 0; j < n; j++)
+			if (rand() % 2 == 1)
+				indices_for_agreement.push_back(j);
+
 		bitset<n> agreed_bits;
-		size_t agreed_bits_size = 0;
-		knuth2<n>(alice_bit_sent, alice_sending_basis, bob_receiving_basis, bob_bit_received, agreed_bits, agreed_bits_size);
+
+		knuth2<n>(indices_for_agreement, alice_bit_sent, alice_sending_basis, bob_receiving_basis, bob_bit_received, agreed_bits);
 
 		cout << "Bit sent:     " << alice_bit_sent << endl;
 		cout << "Bit received: " << bob_bit_received << endl;
 
+		cout << "Test indices: ";
+
+		for (size_t j = 0; j < indices_for_agreement.size(); j++)
+			cout << indices_for_agreement[j] << ' ';
+
+		cout << endl;
+
 		cout << "Agreed bits:  ";
 
-		// In case agreed_bits_size < agreed_bits.size()
-		for(size_t j = 0; j < agreed_bits_size; j++)
+		for(size_t j = 0; j < indices_for_agreement.size(); j++)
 			cout << agreed_bits[j];
 		
 		cout << endl << endl;
